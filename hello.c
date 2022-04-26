2073b0cb42f03a8be768e84f1dbb6aa36c9e1981
solitary <wtfuwakeup8@gmail.com>
2022-04-26 14:33:37 +0800

    change the hell.c, add struct Node

--- hello.c
+++ hello.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 
+typedef struct Node{
+	struct Node * rnode;
+	int data;
+} Node;
+
 int main(void){
 	printf("helloworld");
 	int a = 2;
 	int b = 3;
 	puts("what can I help you");
 	return 0;
-
 }
